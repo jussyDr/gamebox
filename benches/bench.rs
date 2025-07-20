42f97b6a6ccdca28e42a22ab60f0612066f82c7d
Justin de Ruiter <justinderuiter@ziggo.nl>
2025-07-20 11:51:36 +0200

    Add map reading benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,1 +1,13 @@
+#![feature(test)]
 
+extern crate test;
+
+use gamebox::{Challenge, read_file};
+use test::Bencher;
+
+#[bench]
+fn read_map(b: &mut Bencher) {
+    b.iter(|| {
+        let _map: Challenge = read_file("tests/files/map/Alive.Map.Gbx").unwrap();
+    });
+}
